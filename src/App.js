82877b164f23a55f56a4a47f457bea9d7a1b9378
fanzheming <506954808@qq.com>
2020-09-15 17:02:58 +0800

    header制作

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { memo } from 'react'
+import { HashRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+
+import routes from './router';
+
+
+import  AppHeader from '@/components/app-header';
+import  AppFooter from '@/components/app-footer';
+
 
 export default memo(function App() {
     return (
-        <div>
-            
-        </div>
+        <HashRouter>
+            <AppHeader/>
+            {renderRoutes(routes)}
+            <AppFooter/>
+        </HashRouter>
     )
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//重置css
+import '@/assets/css/reset.css'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
