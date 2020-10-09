968d64075ee1df7d68dd911542212797e1a12b46
fanzheming <506954808@qq.com>
2020-10-09 17:40:08 +0800

    feat:加上antd轮播图

--- src/pages/discover/c-pages/recommend/c-cpns/top-banner/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/top-banner/index.js
@@ -1,9 +1,13 @@
-import React, { memo, useEffect} from 'react'
+import React, { memo, useEffect, useRef } from 'react'
 import { useSelector, useDispatch, shallowEqual } from 'react-redux'
-import { getTopBannerAction } from './store/actionCreators'
+import { getTopBannerAction } from '../../store/actionCreators'
+
+import { Carousel } from 'antd';
 import { BannerWrapper, BannerLeft, BannerRight, BannerControl } from './style'
 
 export default memo(function TopBanner() {
+
+    const bannerRef = useRef()
 
     const { topBanners } = useSelector(state => ({
         topBanners: state.getIn(['recommend', 'topBanners'])
@@ -14,12 +18,28 @@ export default memo(function TopBanner() {
     useEffect(() => {
         dispatch(getTopBannerAction());
     }, [dispatch])
-    
+
     return (
         <BannerWrapper>
             <div className='banner wrap-v2'>
-                <BannerLeft></BannerLeft>
+                <BannerLeft>
+                    <Carousel effect="fade" autoplay ref={bannerRef}>
+                        {
+                            topBanners.map((item, index) => {
+                                return (
+                                    <div className="banner-item" key={item.imageUrl}>
+                                        <img className="image" src={item.imageUrl} alt={item.typeTitle} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </Carousel>
+                </BannerLeft>
                 <BannerRight></BannerRight>
+                <BannerControl>
+                    <button className="btn left" onClick={e => bannerRef.current.prev()}></button>
+                    <button className="btn right" onClick={e => bannerRef.current.next()}></button>
+                </BannerControl>
             </div>
         </BannerWrapper>
     )
