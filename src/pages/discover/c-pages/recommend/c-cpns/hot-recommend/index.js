97f36c50239124d071f267fc0257c97bf7c14e3d
fanzheming <506954808@qq.com>
2020-10-12 17:45:53 +0800

    feat: 热门推荐小组件的制作

--- src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -1,10 +1,11 @@
 import React, { memo } from 'react'
 import { HotRecommendWrapper } from './style'
+import ThemeHeaderRCM from '@/components/theme-header-rcm'
 
 export default memo(function HotRecommend() {
     return (
         <HotRecommendWrapper>
-            <h2>123</h2>
+            <ThemeHeaderRCM title="热门推荐" keywords={["华语","流行","民谣","摇滚","电子"]}></ThemeHeaderRCM>
         </HotRecommendWrapper>
     )
 })
